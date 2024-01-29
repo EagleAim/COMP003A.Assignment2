e974c160273d6cebdeaea6fb64bc0fe2dceee2d7
Emma <Emma@EMMAPC>
2024-01-28 18:42:12 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * Author: Logan King
+ * Course: Comp-003A
+ * Purpouse: The purpouse of this code is to show that we learned how to generate console code in VS.
+ */
+
+
+Console.Title = "COMP-003A - Assignment 2";
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+Console.ForegroundColor = ConsoleColor.White;
+
+//strings
+
+string firstName = Console.ReadLine();
+string middleName = Console.ReadLine();
